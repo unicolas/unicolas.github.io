c4e2d5bf2f65f5c9a590a72d6c43388fd8162102
Nicolás Urquiola <nicourquiola@gmail.com>
2024-01-15 15:15:27 -0300

    Style blockquote as inline notif

--- src/lib/components/Blockquote.svelte
+++ src/lib/components/Blockquote.svelte
@@ -1,9 +1,14 @@
-<div {...$$restProps} class="blockquote">
-  <slot />
+<div
+  {...$$restProps}
+  class="blockquote cds--inline-notification cds--inline-notification--low-contrast cds--inline-notification--info-square"
+>
+  <div class="cds--inline-notification__details">
+    <slot />
+  </div>
 </div>
 
 <style lang="scss">
   .blockquote {
-    font-style: italic;
+    max-width: unset;
   }
 </style>
